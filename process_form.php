d05ae699e66f9ca7736e92b3f1e3c19cdf68b4c0
Bruneitor123 <brunitoaas@gmail.com>
2023-04-17 21:07:12 -0400

    fixing for process_form check for random countries

--- process_form.php
+++ process_form.php
@@ -94,7 +94,7 @@ if (in_array($modalidad, $modalidades)) {
   }
 
 
-  $paises_general_sinvalue = array(
+  $paises_general = array(
     'Estados Unidos de América',
     'Federación de Rusia',
     'República Popular China',
@@ -111,27 +111,8 @@ if (in_array($modalidad, $modalidades)) {
     'Estado del Japón',
     'Estados Unidos Mexicanos'
 );
-$paises_general = array_combine($paises_general_sinvalue, $paises_general_sinvalue);
 
-$paises_general_valores = array(
-  'Estados Unidos de América' => 0,
-  'Federación de Rusia' => 0,
-  'República Popular China' => 0,
-  'Reino Unido de Gran Bretaña e Irlanda del Norte' => 0,
-  'República Francesa' => 0,
-  'Ucrania (Україна)' => 0,
-  'Reino de Arabia Saudita' => 0,
-  'Emirato Islámico de Afganistán' => 0,
-  'Estados Unidos de Venezuela' => 0,
-  'República de la India' => 0,
-  'Confederación Suiza' => 0,
-  'Estado de Israel' => 0,
-  'República de Haití' => 0,
-  'Estado del Japón' => 0,
-  'Estados Unidos Mexicanos' => 0
-);
-
-$delegados_nacionales_sinvalue = array(
+$delegados_nacionales = array(
   'Departamento de Oruro',
   'Departamento de Cochabamba',
   'Departamento de La Paz',
@@ -148,28 +129,9 @@ $delegados_nacionales_sinvalue = array(
   'Estado del Japón',
   'Estados Unidos Mexicanos'
 );
-$delegados_nacionales = array_combine($delegados_nacionales_sinvalue, $delegados_nacionales_sinvalue);
 
 
-$delegados_nacionales_valores = array(
-  'Departamento de Oruro' => 0,
-  'Departamento de Cochabamba' => 0,
-  'Departamento de La Paz' => 0,
-  'Departamento de Potosí' => 0,
-  'Departamento de Chuquisaca' => 0,
-  'Departamento de Tarija' => 0,
-  'Departamento de Santa Cruz' => 0,
-  'Departamento del Beni' => 0,
-  'Departamento de Pando' => 0,
-  'República de la India' => 0,
-  'Confederación Suiza' => 0,
-  'Estado de Israel' => 0,
-  'República de Haití' => 0,
-  'Estado del Japón' => 0,
-  'Estados Unidos Mexicanos' => 0
-);
-
-$paises_comision_paz_sinvalue = array(
+$paises_comision_paz = array(
   'Ucrania (Україна)',
   'Reino de Arabia Saudita',
   'Emirato Islámico de Afganistán',
@@ -186,79 +148,37 @@ $paises_comision_paz_sinvalue = array(
   'República de Corea',
   'República de Sudáfrica'
 );
-$paises_comision_paz = array_combine($paises_comision_paz_sinvalue, $paises_comision_paz_sinvalue);
-
-
-$paises_comision_paz_valores = array(
-  'Ucrania (Україна)' => 0,
-  'Reino de Arabia Saudita' => 0,
-  'Emirato Islámico de Afganistán' => 0,
-  'Estados Unidos de Venezuela' => 0,
-  'República de la India' => 0,
-  'Confederación Suiza' => 0,
-  'Estado de Israel' => 0,
-  'República de Haití' => 0,
-  'Estado del Japón' => 0,
-  'Estados Unidos Mexicanos' => 0,
-  'República Italiana' => 0,
-  'República Federal de Alemania' => 0,
-  'Países Bajos (Holanda)' => 0,
-  'República de Corea' => 0,
-  'República de Sudáfrica' => 0
-);
 
 
   // prepare and execute query to get count of each option value
-  $stmt = $conn->prepare("SELECT equipo_diplomatico, COUNT(1) FROM bassmun_registro WHERE foro = '$foro' GROUP BY equipo_diplomatico");
+  $stmt = $conn->prepare("SELECT equipo_diplomatico FROM bassmun_registro WHERE foro = '$foro' GROUP BY equipo_diplomatico");
   $stmt->execute();
   $result = $stmt->get_result();
 
       if ($foro == 'Comité de Tierra y Territorio, Recursos Naturales y Hoja de la Coca (Foro Nacional)') {
           while ($row = $result->fetch_assoc()) {
-              $option_value = $row['equipo_diplomatico'];
-              $count = $row['COUNT(1)'];
-              if (array_key_exists($option_value, $delegados_nacionales_valores)) {
-                  $delegados_nacionales_valores[$option_value] = $count;
-              }
+              $paises_ya_elegidos[] = $row['equipo_diplomatico'];
           }
-          foreach ($delegados_nacionales_valores as $option => $count) {
-              if ($count >= 1) {
-                unset($delegados_nacionales[$option]);
-                $random_index = array_rand($delegados_nacionales);
-                $valor_pais_random = $delegados_nacionales[$random_index];
-              }
-          }
+          $paises_disponibles = array_diff($delegados_nacionales, $paises_ya_elegidos);
+          $random_index = array_rand($paises_disponibles);
+          $valor_pais_random = $paises_disponibles[$random_index];
+
       } else if ($foro == 'Comisión de Consolidación de la Paz') {
           while ($row = $result->fetch_assoc()) {
-              $option_value = $row['equipo_diplomatico'];
-              $count = $row['COUNT(1)'];
-              if (array_key_exists($option_value, $paises_comision_paz_valores)) {
-                  $paises_comision_paz_valores[$option_value] = $count;
-              }
-          }
-          foreach ($paises_comision_paz_valores as $option => $count) {
-              if ($count >= 1) {
-                unset($paises_comision_paz[$option]);
-                $random_index = array_rand($paises_comision_paz);
-                $valor_pais_random = $paises_comision_paz[$random_index];
-              }
-          }
+            $paises_ya_elegidos[] = $row['equipo_diplomatico'];
+        }
+        $paises_disponibles = array_diff($paises_comision_paz, $paises_ya_elegidos);
+        $random_index = array_rand($paises_disponibles);
+        $valor_pais_random = $paises_disponibles[$random_index];
+
       } else {
           while ($row = $result->fetch_assoc()) {
-            $option_value = $row['equipo_diplomatico'];
-            $count = $row['COUNT(1)'];
-            if (array_key_exists($option_value, $paises_general_valores)) {
-                $paises_general_valores[$option_value] = $count;
-            }
+            $paises_ya_elegidos[] = $row['equipo_diplomatico'];
         }
-        foreach ($paises_general_valores as $option => $count) {
-            if ($count >= 1) {
-              unset($paises_general[$option]);
-              $random_index = array_rand($paises_general);
-              $valor_pais_random = $paises_general[$random_index];            
-            }
+        $paises_disponibles = array_diff($paises_general, $paises_ya_elegidos);
+        $random_index = array_rand($paises_disponibles);
+        $valor_pais_random = $paises_disponibles[$random_index];
         }
-      }
   
   $equipo_diplomatico = $valor_pais_random;
 }
@@ -298,7 +218,7 @@ if ($_FILES['comprobante']['error'] == UPLOAD_ERR_OK) {
     }
   } else {
     // invalid file type or size
-    echo 'Error: Tipo de archivo inválido (Solo PDF) o archivo con tamaño muy grande (Hasta 1MB)';
+    echo 'Error: Tipo de archivo inválido (Solo PDF) o archivo con tamaño muy grande (Hasta 1MB), por favor vuelve atrás.';
   }
 } else {
   // error occurred while uploading the file
