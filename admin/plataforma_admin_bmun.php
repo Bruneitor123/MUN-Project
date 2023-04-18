9d9c757c112a01d8271280980520d76625335e8f
Bruneitor123 <brunitoaas@gmail.com>
2023-04-17 23:05:56 -0400

    testing new plataforma admin

--- admin/plataforma_admin_bmun.php
+++ admin/plataforma_admin_bmun.php
@@ -88,7 +88,7 @@ if($decrypted_value === $cookie_value)  {
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48">
         <use xlink:href="#home"></use>
       </svg>
-      <a href="plataforma_admin_bmun.php">DB BassMUN 2023</a>
+      <a href="">Home</a>
     </li>
     
     <li class="nav__items ">
@@ -123,6 +123,107 @@ if($decrypted_value === $cookie_value)  {
       <p><a href="/" class="btn btn-primary">Cerrar Sesión</a></p>
     </main>
   </div>
+
+
+<!-- Create a table element with the Bootstrap table class -->
+<table class="table table-striped table-hover">
+
+  <!-- Add a table header row with the Bootstrap table header class -->
+  <thead class="table-dark">
+    <tr class='text-center'>
+      <th>Nro</th>
+      <th>Nombre Completo</th>
+      <th>Carnet con Extensión</th>
+      <th>Celular</th>
+      <th>Correo Electrónico</th>
+      <th>Foro</th>
+      <th>Equipo Diplomático</th>
+      <th>Nombre Comprobante</th>
+      <th>Identificador Único</th>
+      <th>Inscripción Confirmada <br>(1 = Si ; 0 = No)</th>
+    </tr>
+  </thead>
+
+  <tbody>
+    <?php
+      // Replace the database credentials with your own
+
+      // Create a connection to the database
+      $conn = new mysqli($servername, $username, $password, $dbname);
+
+      // Check for errors in the connection
+      if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+      }
+
+      // Create a query to retrieve the data from the database
+      $sql = "SELECT Nro, nombre_completo, carnet_ext, celular, correo, foro, equipo_diplomatico, nom_comprobante, uniq_ident, Confirmado FROM bassmun_registro";
+
+      // Execute the query and retrieve the results
+      $result = $conn->query($sql);
+
+      // Check if any rows were returned
+      if ($result->num_rows > 0) {
+        // Output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td class='text-decoration-underline text-wrap'><button type='button' class='btn btn-info' id='modinfo' onclick='modifyuser()'>" . $row["Nro"] . "</button></td>";
+            echo "<td>" . $row["nombre_completo"] . "</td>";
+            echo "<td>" . $row["carnet_ext"] . "</td>";
+            echo "<td>" . $row["celular"] . "</td>";
+            echo "<td>" . $row["correo"] . "</td>";
+            echo "<td>" . $row["foro"] . "</td>";
+            echo "<td>" . $row["equipo_diplomatico"] . "</td>";
+            echo "<td>" . $row["nom_comprobante"] . "</td>";
+            echo "<td>" . $row["uniq_ident"] . "</td>";
+            echo "<td>" . $row["Confirmado"] . "</td>";
+            echo "</tr>";
+        }
+      } else {
+            echo "<tr><td colspan='9'>0 results</td></tr>";
+      }
+
+      // Close the connection to the database
+      $conn->close();
+    ?>
+  </tbody>
+</table>
+
+<!-- Include the Bootstrap 5 JavaScript library -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+
+<script>
+  function modifyuser() {
+    var row = event.target.parentNode.parentNode;
+    var cells = row.getElementsByTagName("td");
+
+    var params = "";
+    for (var i = 0; i < cells.length; i++) {
+        var key = "";
+        var header = document.getElementsByTagName("th")[i].innerHTML;
+        if (header.includes("Inscripción Confirmada")) {
+            key = "confirmado";
+        } else {
+            key = header.replace(/\s/g, "_");
+        }
+        if (key == "Nro") {
+            continue;
+        }
+        var value = cells[i].innerHTML;
+        params += encodeURIComponent(key) + "=" + encodeURIComponent(value) + "&";
+    }
+
+    params = params.slice(0, -1); // Remove last '&'
+    var url = "/admin/editar_usuario.php?" + params;
+
+    window.location.href = url;
+  }
+
+
+
+
+
+</script>
 
 <div class="container-fluid text-white" style="background: #061429;">
         <div class="container text-center">
